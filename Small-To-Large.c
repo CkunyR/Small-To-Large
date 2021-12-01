06936c33d78ae8e23df2c92d14a10350422fae7e
Zhenkun Guo <1608229093@qq.com>
2021-12-01 17:22:59 +0800

    从小到大

---
+++ Small-To-Large.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d%d%d", &a, &b, &c);
+	//算法实现
+	//a中放最大值
+	//b中次之
+	//c中放最小值
+	if (a < b)
+	{
+		int tmp = a;
+		a = b;
+		b = tmp;
+	}
+	if (a < c)
+	{
+		int tmp = a;
+		a = c;
+		c = tmp;
+	}
+	if (b < c)
+	{
+		int tmp = b;
+		b = c;
+		c = tmp;
+	}
+	printf("%d %d %d", a, b, c);
+
+
+	return 0;
+}
